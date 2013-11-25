bb13f85480b956873c65e001cd5dbdf6602d5500
Esther Sun Jang <esthersunjang@gmail.com>
2013-11-24 21:56:49 -0800

    Initial Rails Project Scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crumblr::Application.config.secret_key_base = '35a66d953fab439983b1951e578caca61aeae722c42803c7c042120bf29a031169cd0ec5333fc2296fd981f530fcd28c625700dbeee00d0d27aad6948fa9b0ce'
